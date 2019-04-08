50bd98a207aafaaa432f5e8735346738a7de3668
Peter Kosyh <p.kosyh@gmail.com>
2019-04-08 20:48:36 +0300

    remove disquss

--- iframe.sh
+++ iframe.sh
@@ -2,6 +2,6 @@ cat index.html | sed -e '/<link href="css\/styles.css"/a <link href="css\/styles
 sed -e '/href="lib\/mp.css">/a <link rel="stylesheet" href="lib\/mp-iframe.css">' -e '/<body>/a <div class="status TextGrid"><a href="\/index-iframe.html"><b>назад </b></a></div>' -e 's|index\.html|index-iframe\.html|g' games/index.html> games/index-iframe.html
 for f in games/*; do
 	if test -f $f/index.html; then
-		cat $f/index.html | sed -e '/<link rel="stylesheet"/a <link rel="stylesheet" href="..\/style-iframe.css" type="text/css" media="all">' -e '/<body>/a <div class="status TextGrid"><a href="\/index-iframe.html"><b>назад </b></a></div>' -e 's|index\.html|index-iframe\.html|g' > $f/index-iframe.html
+		cat $f/index.html | sed -e '/<link rel="stylesheet"/a <link rel="stylesheet" href="..\/style-iframe.css" type="text/css" media="all">' -e '/<body>/a <div class="status TextGrid"><a href="\/index-iframe.html"><b>назад </b></a></div>' -e 's|index\.html|index-iframe\.html|g' -e '/<div id="disqus_thread">/,/<\/noscript>/d' > $f/index-iframe.html
 	fi
 done
