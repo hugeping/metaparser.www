37ea8c84beeac3def7d9f49adf621955a0faaf54
Peter Kosyh <p.kosyh@gmail.com>
2019-04-08 16:21:11 +0300

    iframe ready?

---
+++ iframe.sh
@@ -0,0 +1,7 @@
+cat index.html | sed -e '/<link href="css\/styles.css"/a <link href="css\/styles-iframe.css" rel="stylesheet">' -e 's|index\.html|index-iframe\.html|g'> index-iframe.html
+sed -e '/href="lib\/mp.css">/a <link rel="stylesheet" href="lib\/mp-iframe.css">' -e '/<body>/a <div class="status TextGrid" style="background-color:inherit;"><a href="\/index-iframe.html"><b>&lt;назад</b></a></div>' -e 's|index\.html|index-iframe\.html|g' games/index.html> games/index-iframe.html
+for f in games/*; do
+	if test -f $f/index.html; then
+		cat $f/index.html | sed -e '/<link rel="stylesheet"/a <link rel="stylesheet" href="..\/style-iframe.css" type="text/css" media="all">' -e '/<body>/a <div class="status TextGrid" style="background-color:inherit;"><a href="\/index-iframe.html"><b>&lt;назад</b></a></div>' -e 's|index\.html|index-iframe\.html|g' > $f/index-iframe.html
+	fi
+done
