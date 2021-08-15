beba62185eb1b064ab7ca059850142661b9d3d3d
peter <p.kosyh@gmail.com>
2021-08-15 11:38:29 +0300

    reinstead link

--- iframe.sh
+++ iframe.sh
@@ -1,4 +1,4 @@
-cat index.html | sed -e '/<link href="css\/styles.css"/a <link href="css\/styles-iframe.css" rel="stylesheet">' -e 's|index\.html|index-iframe\.html|g' -e '/<div id="disqus_thread">/,/<\/noscript>/d'  -e '/<div style="overflow-x: visible" class="donate">/,/^<\/div>/d'> index-iframe.html
+cat index.html | sed -e '/<link href="css\/styles.css"/a <link href="css\/styles-iframe.css" rel="stylesheet">' -e 's|index\.html|index-iframe\.html|g' -e '/<div id="disqus_thread">/,/<\/noscript>/d'  -e '/<div style="overflow-x: visible" class="donate">/,/^<\/div>/d' -e '/^<p>Для большего удобства.\+<\/p>$/d' > index-iframe.html
 sed -e '/href="lib\/mp.css">/a <link rel="stylesheet" href="lib\/mp-iframe.css">' -e '/<body>/a <div class="status TextGrid"><a href="\/index-iframe.html"><b>назад </b></a></div>' -e 's|index\.html|index-iframe\.html|g' games/index.html> games/index-iframe.html
 for f in games/*; do
 	if test -f $f/index.html; then
